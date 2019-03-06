1668a0c10250542a4c7fbde2b69a5f3cace660a7
Maciej Pasternacki <maciej@pasternacki.net>
2019-03-06 21:03:37 +0000

    Update entrypoint-etcd.py

--- entrypoint-etcd.py
+++ entrypoint-etcd.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+print('lorem')
 import etcd
-
+print('ipsum')
 is_ready = False
+print('dolor')
 client = etcd.Client(host='etcd', port=2379)
-
+print('sit')
 while not is_ready:
+    print('amet')
     print(is_ready)
     print("dupa")
     try:
         print('dupa')
         os.environ['POSTGRES_USER'] = client.read('/db_data/user').value
+        print('i tak dalej')
         os.environ['POSTGRES_DB'] = client.read('/db_data/user').value
         os.environ['POSTGRES_PASSWORD'] = client.read('/db_data/pswd').value
         is_ready = True
